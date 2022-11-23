80966388e0eb7d0fd2b8d8776e3c8fa9adf8d62a
AlfieriChou <alfierichou@gmail.com>
2022-11-23 17:18:17 +0800

    chore: update random

--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -1,15 +1,8 @@
-const LIMIT = 5
+const LIMIT = 100
 
 module.exports = Model => {
   return class extends Model {
     static async remoteRandom (ctx) {
-      const count = await ctx.models.Restaurant.count({
-        where: {
-          status: 'DEFAULT'
-        },
-        distinct: true
-      })
-      const offset = count > LIMIT ? Math.floor(Math.random() * count) + 1 : 0
       const restaurants = await ctx.models.Restaurant.find({
         where: {
           status: 'DEFAULT'
@@ -17,7 +10,6 @@ module.exports = Model => {
         order: [
           ['seq', 'ASC']
         ],
-        offset: offset > LIMIT ? offset - LIMIT : offset,
         limit: LIMIT
       }, ctx)
       const restaurant = restaurants[restaurants.length - 1]
@@ -25,12 +17,15 @@ module.exports = Model => {
         ctx.throw(400, '数据缺失')
       }
       const { seq } = restaurant
-      const list = restaurants.map(item => {
-        return new Array(seq - item.seq + 1).fill(1).map(_item => {
-          return item
+      const list = restaurants
+        .map(item => {
+          return new Array(seq - item.seq + 1).fill(1).map(_item => {
+            return item
+          })
         })
-      }).flat()
-      const randomRestaurant = list[Math.floor(Math.random() * list.length) + 1]
+        .flat()
+        .sort(() => { return Math.random() > 0.5 ? -1 : 1 })
+      const randomRestaurant = list[Math.floor(Math.random() * list.length)]
       await ctx.models.Restaurant.increment({
         seq: 1
       }, {
